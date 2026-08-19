77fc1c7c8b15f49229b2103296fde728cf957b2d
Nishie Yui <cdash0103ktys@i.softbank.jp>
2026-08-19 12:38:52 +0900

    feat: add highway merge step 1 baseline experiment

---
+++ experiments/highway_merge_v2/step01_baseline/config.py
@@ -0,0 +1,7 @@
+"""Step 1 conditions for the highway-merge-v2 free-flow baseline."""
+
+DEFAULT_MAIN_RATES = [200, 400, 600]
+DEFAULT_RAMP_RATES = [100, 200, 300]
+DEFAULT_SEEDS = [7, 42, 99]
+DEFAULT_DURATION_S = 120.0
+DEFAULT_CLEARANCE_TIME_S = 180.0
